9a11d6d20c8581f1b9322aaf39cf74ee99c7e47d
husain <h.y.patanwala@gmail.com>
2026-04-23 12:35:38 -0700

    update fireprojectile tower script
    Started working on the mechanics for tower range, projectile lobbing, raycasting to check if we are in the allowed current fire range, etc

--- Assets/Scripts/Tower/FireProjectile.cs
+++ Assets/Scripts/Tower/FireProjectile.cs
@@ -2,26 +2,48 @@ using UnityEngine;
 
 public class FireProjectile : MonoBehaviour
 {
-    public Rigidbody projectile;
-    public Transform launchPoint;
-    public float speed = 1f;
+    [Header("Projectile")]
+    public Rigidbody m_projectile;
+    public Transform m_launchPoint;
+    public float m_speed = 1f;
+
+    [Header("Raycast Projectile Targeting")]
+    private float m_raycastMaxDistance = 500f;
+    /*this all layers is just for testing purposes
+     but in practice this isn't what we want. 
+     we need to specifically mask everything but ground so our 
+     ray doesn't get short by hitting enemies or friendlies*/
+    private LayerMask m_raycastLayers = Physics.AllLayers;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (projectile == null)
-            {
-                Debug.LogError("FireProjectile: Projectile prefab is not assigned.");
-                return;
-            }
+            if (m_projectile == null) { return; }
 
-            Transform spawn = launchPoint != null ? launchPoint : transform;
-
-            Rigidbody p = Instantiate(projectile, spawn.position, spawn.rotation);
-            p.linearVelocity = spawn.forward * speed;
-
-            Debug.Log($"Fired projectile from {spawn.name} at speed {speed}");
+            Transform spawn = m_launchPoint != null ? m_launchPoint : transform;
+            Rigidbody p = Instantiate(m_projectile, spawn.position, spawn.rotation);
+            p.linearVelocity = spawn.forward * m_speed;
         }
+    }
+
+    /*out means that var must be assigned before function returns*/
+    bool TryGetMouseWorldPoint(out Vector3 point)
+    {
+        point = default;
+
+        /*find the main camera*/
+        Camera cam = Camera.main;
+        if (cam == null) return false;
+
+        /*convert the mouse pos to a ray so we can raycast*/
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        /*raycast needs to ignore other colliders so we just check for dist to ground point we want*/
+        if (!Physics.Raycast(ray, out RaycastHit hit, 1000f, ~0, QueryTriggerInteraction.Ignore))
+            return false;
+
+        point = hit.point;
+        return true;
     }
 }
